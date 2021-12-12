8a0b8edd7b5fd10295826173abdefea7e518e84b
Andressa Ruy <andressa.ruy@aquiris.com.br>
2021-12-12 18:01:11 -0300

    add to list

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,16 +1,38 @@
-import axios from 'axios'
-import React, { useEffect, useState } from "react"
-import { Form } from 'react-bootstrap'
-import HeaderDashboard from '../HeaderDashboard/HeaderDashboard'
-import Loading from "../Loading/Loading"
-import TodoList from '../TodoList/TodoList'
-import "./Dashboard.css"
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { v4 as uuid } from 'uuid';
+import HeaderDashboard from '../HeaderDashboard/HeaderDashboard';
+import Loading from "../Loading/Loading";
+import TodoList from '../TodoList/TodoList';
+import "./Dashboard.css";
 
 
 
 const Dashboard = ({ x }) => {
   // const navigate = useNavigate()
+  const [input, setInput] = useState("");
 
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    let newTask = {
+      "userId": 1, // get current user
+      "id": uuid(),
+      "title": input,
+      "completed": false
+    }
+
+    // beginning of array - keeping as an example
+    //setTodos(todos => [newTask, ...todos])
+
+    setTodos([...todos, newTask])
+    console.log(newTask);
+  }
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  }
 
   const [todos, setTodos] = useState(null);
 
@@ -27,7 +49,7 @@ const Dashboard = ({ x }) => {
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/todos")
       .then((result) => {
-        setTodos(result.data)
+        setTodos(result.data.slice(0, 5))
       });
   }, []) // [] only fires one time when the component loads
 
@@ -35,13 +57,28 @@ const Dashboard = ({ x }) => {
     <div>
       <HeaderDashboard />
 
-      <div className="box">
-        <Form >
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Control type="text" placeholder="Add to Inbox" />
-          </Form.Group>
+      <Container className="box">
+        <Form onSubmit={handleSubmit}>
+          <Row className="align-items-center">
+            <Col xs="10">
+              <Form.Control
+                className="mb-2"
+                id="inlineFormInput"
+                placeholder="Add to Inbox"
+                value={input}
+                onChange={handleChange}
+
+              />
+            </Col>
+
+            <Col xs="auto">
+              <Button type="submit" className="mb-2">
+                Submit
+              </Button>
+            </Col>
+          </Row>
         </Form>
-      </div>
+      </Container>
 
 
       {todos ? <TodoList todos={todos} onUpdateTodo={onUpdateTodo} /> : <Loading />}
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,18 +1,21 @@
 import React from "react";
-import {Checkbox} from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 
 const TodoList = ({ todos, onUpdateTodo }) => {
     return (
-        <div className="box">
-            <ul className="list-group">
-                {todos.map((todo) =>
-                    <li className="list-group-item d-flex justify-content-between align-items-center">
-                        {todo.title}
-                        <input type="checkbox" checked={todo.completed} onChange={() => onUpdateTodo(todo)}></input>
-                    </li>
-                )}
-            </ul>
-        </div>
+        <Row>
+            <div className="box">
+                <ul className="list-group">
+                    {todos.map((todo) =>
+                        <li className="list-group-item d-flex justify-content-between align-items-center">
+                            {todo.title}
+                            <input type="checkbox" checked={todo.completed} onChange={() => onUpdateTodo(todo)}></input>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </Row>
+
 
     )
 }
