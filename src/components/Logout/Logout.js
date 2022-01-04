e41dd5d69f2db090eddfbac82c9088bd288e9186
Andressa Ruy <andressa.ruy@aquiris.com.br>
2022-01-04 17:59:38 -0300

    improve logout screen

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,7 +1,8 @@
 import { React } from "react";
-import { Button, Row } from "react-bootstrap";
+import { Button, Row, Container, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../../Utils/auth";
+import '../Login/Login.css';
 
 function Logout() {
     let navigate = useNavigate()
@@ -13,12 +14,18 @@ function Logout() {
 
     return (
         <div>
+            <Container>
+            <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
             <Row className="mt-1">
                 <p> Sure you want to logout?</p>
                 <Button onClick={onLogout}>
                     Logout
                 </Button>
             </Row>
+                </Col>
+            
+            </Container>
+            
 
         </div >
     )
