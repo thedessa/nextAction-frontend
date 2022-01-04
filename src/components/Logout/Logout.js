afa3ba9af2afc05c4f5ef8aacdd3c391d4ac9d36
Andressa Ruy <andressa.ruy@aquiris.com.br>
2022-01-04 18:00:46 -0300

    improve logout

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { Button, Row, Container, Col } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../../Utils/auth";
 import '../Login/Login.css';
@@ -15,17 +15,17 @@ function Logout() {
     return (
         <div>
             <Container>
-            <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
-            <Row className="mt-1">
-                <p> Sure you want to logout?</p>
-                <Button onClick={onLogout}>
-                    Logout
-                </Button>
-            </Row>
-                </Col>
-            
+                <Row className="mt-1">
+                    <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
+                        <p> Sure you want to logout?</p>
+                        <Button onClick={onLogout}>
+                            Logout
+                        </Button>
+                    </Col>
+                </Row>
+
             </Container>
-            
+
 
         </div >
     )
